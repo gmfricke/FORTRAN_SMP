3d7e2390ea3766940c45cf5f373a0fe89982a08f
Matthew Fricke <matthew@fricke.co.uk>
2024-02-28 08:16:00 -0600

    OpenMP FORTRAN example

---
+++ serial_vecadd.f90
@@ -0,0 +1,25 @@
+! Program to add up elements of an array
+program serial_array_sum
+  USE OMP_LIB
+  implicit none
+
+! Declare variables, N is the size of the array
+  ! Array holds the data to sum
+  INTEGER :: N,i
+  CHARACTER(LEN=100) :: arg
+  REAL, ALLOCATABLE :: a(:), b(:), c(:)
+    
+  call GET_COMMAND_ARGUMENT(1, arg)
+  read(arg,*) N
+  ALLOCATE(a(N))
+  ALLOCATE(b(N))
+  ALLOCATE(c(N))
+  
+  call RANDOM_NUMBER(b)
+  call RANDOM_NUMBER(c)
+    
+  do i=1,N
+    a(i) = b(i) + c(i)
+  end do
+  
+end program serial_array_sum
